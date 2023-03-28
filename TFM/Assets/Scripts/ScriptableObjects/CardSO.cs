871d417ff20603fc0eae5e56f7039babd4535d91
Laia <laia.fustercorral@gmail.com>
2023-03-28 20:35:31 +0200

    Added Card ScriptableObjects basic definitions

---
+++ TFM/Assets/Scripts/ScriptableObjects/CardSO.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class CardSO : ScriptableObject
+{
+    public CardType type;
+    public string cardName;
+    public Effect[] effects;
+}
+
+public enum CardType
+{
+    Character, Event, Equipment
+}
+
+public enum StatType
+{
+    Health, Attack, Shield, Coins
+}
+
+[Serializable]
+public struct Effect
+{
+    public StatType affectedStat;
+    public float affectionAmount;
+    public string effectText;
+}
+
+public enum Rarity
+{
+    Common, Uncommon, Rare
+}
---
+++ TFM/Assets/Scripts/ScriptableObjects/CharacterCardSO.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character Card", menuName = "Cards/Character", order = 1)]
+public class CharacterCardSO : CardSO
+{
+    public float baseHealth;
+    public float baseAttack;
+    public float baseShield;
+    public int baseCoins;
+
+    private float currentHealth;
+    private float currentAttack;
+    private float currentShield;
+    private int currentCoins;
+}
