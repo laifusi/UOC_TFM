047a7ae4e4c387d144f47c54c87b10f70f699d1d
Laia <laia.fustercorral@gmail.com>
2023-03-28 20:35:51 +0200

    More ScriptableObject definitions

---
+++ TFM/Assets/Scripts/ScriptableObjects/EquipmentCardSO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment Card", menuName = "Cards/Equipment", order = 3)]
+public class EquipmentCardSO : CardSO
+{
+    public Rarity rarity;
+    public int cost;
+}
---
+++ TFM/Assets/Scripts/ScriptableObjects/EventCardSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Event Card", menuName = "Cards/Event", order = 2)]
+public class EventCardSO : CardSO
+{
+    public float health;
+    public float attack;
+    public Rarity rarity;
+}
