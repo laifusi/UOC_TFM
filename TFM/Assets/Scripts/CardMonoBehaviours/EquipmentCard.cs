5d7a3a97be9dfbcaa19350c9dce8743b5fb95ab1
Laia <laia.fustercorral@gmail.com>
2023-05-26 20:48:17 +0200

    A few adjustments and new events

--- TFM/Assets/Scripts/CardMonoBehaviours/EquipmentCard.cs
+++ TFM/Assets/Scripts/CardMonoBehaviours/EquipmentCard.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EquipmentCard : Card<EquipmentCardSO>
 {
     [SerializeField] TMP_Text costText;
     [SerializeField] TMP_Text nameText;
     [SerializeField] TMP_Text effectText;
+    [SerializeField] Image affectedStatImage;
 
     public override void PaintCard(EquipmentCardSO cardToPaint)
     {
@@ -15,6 +17,7 @@ public class EquipmentCard : Card<EquipmentCardSO>
         nameText.text = card.GetName();
         costText.text = card.GetCost().ToString();
         effectText.text = card.GetEffectString();
+        affectedStatImage.sprite = IconManager.Instance.GetStatSprite(card.GetEffects()[0].affectedStat);
     }
 
     public int GetCost()
